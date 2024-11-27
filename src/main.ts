a29b59cc8007eef05d01c04d7b83c17e1676eb17
Luis Rodolfo Navarro <navaluisrodolfo@gmail.com>
2024-11-27 17:37:02 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Payments-ms');
+
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  logger.log(`Payments Microservices running in port ${envs.port}`);
+  await app.listen(envs.port);
+}
+bootstrap();
