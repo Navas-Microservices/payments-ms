09e4e599bb306197e3441f28cbe80be6a6e15aa7
Luis Rodolfo Navarro <navaluisrodolfo@gmail.com>
2024-11-29 07:36:14 -0300

    changes to be committed:  # Changes that are staged and will be committed with the next git commit command.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
@@ -12,14 +13,24 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
+  console.log(12345);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    },
+  });
+
+  await app.startAllMicroservices();
 
   logger.log(`Payments Microservices running in port ${envs.port}`);
+
   await app.listen(envs.port);
 }
 bootstrap();
