17ce115b524610d251c02c603211fee63521478b
Luis Rodolfo Navarro <navaluisrodolfo@gmail.com>
2024-11-30 20:53:11 -0300

    Agregar soporte para NATS en el módulo de pagos y mejorar la gestión de sesiones de pago

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,22 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  console.log(12345);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.NATS,
-    options: {
-      servers: envs.natsServers,
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
     },
-  });
+    { inheritAppConfig: true },
+  );
 
   await app.startAllMicroservices();
 
